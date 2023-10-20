9f163cdd7be921ddfa8d1b31c90108d35f3f056d
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-10-21 01:55:37 +0800

    Refactor connect_wallet

--- connect_wallet/components/ConnectButton/index.tsx
+++ connect_wallet/components/ConnectButton/index.tsx
@@ -1,19 +1,25 @@
-"use client"
+"use client";
 import React from "react";
 import Button from "../Button";
-import useWeb3 from './hook'
+import useWeb3 from "./hook";
 
-function ConnectButton({ }: any) {
+function ConnectButton({}: any) {
   const { wallet, connected, connectWallet, disconnectWallet } = useWeb3();
 
   const parseWallet = (key: string) => {
-    return `${key.slice(0, 5)}.....${key.slice(-5)}`
-  }
+    return `${key.slice(0, 5)}.....${key.slice(-5)}`;
+  };
   return (
     <>
-      <Button outlined={!(connected && wallet)} onClick={connected && wallet ? disconnectWallet : connectWallet}>{connected && wallet ? parseWallet(wallet) : "Connect Wallet"}</Button>
+      <Button
+        outlined={!(connected && wallet)}
+        onClick={connected && wallet ? disconnectWallet : connectWallet}
+      >
+        {connected && wallet ? parseWallet(wallet) : "Connect Wallet"}
+      </Button>
     </>
   );
 }
+1;
 
 export default ConnectButton;
