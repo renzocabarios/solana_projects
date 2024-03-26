e0de22f92f4e8e372768c50292c4c2ee5e3fbfce
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-26 19:40:12 +0800

    Update

--- js_scripts/umi/create_account.ts
+++ js_scripts/umi/create_account.ts
@@ -6,17 +6,15 @@
 // } from "@metaplex-foundation/umi";
 // import { base58 } from "@metaplex-foundation/umi/serializers";
 // import { PAYER_PRIVATE_KEY } from "../env";
-// import { createAccount } from "@metaplex-foundation/mpl-toolbox";
-
+// import { createAccount, mplToolbox } from "@metaplex-foundation/mpl-toolbox";
 // // TODO: Fix this
 // const main = async () => {
 //   const decoded = base58.serialize(PAYER_PRIVATE_KEY);
 //   const keypair = UMI_INSTANCE.eddsa.createKeypairFromSecretKey(decoded);
 //   UMI_INSTANCE.use(keypairIdentity(keypair));
-//   UMI_INSTANCE.programs.add({
-//     name: "SystemProgram",
-//     publicKey: publicKey("11111111111111111111111111111111"),
-//   });
+
+//   UMI_INSTANCE.use(mplToolbox());
+
 //   const space = 42;
 //   const KeyPair = generateSigner(UMI_INSTANCE);
 
@@ -24,9 +22,7 @@
 //     newAccount: KeyPair,
 //     lamports: await UMI_INSTANCE.rpc.getRent(space),
 //     space,
-//     programId: UMI_INSTANCE.programs.get(
-//       publicKey("11111111111111111111111111111111")
-//     ).publicKey,
+//     programId: UMI_INSTANCE.programs.get("mplToolbox").publicKey,
 //   }).sendAndConfirm(UMI_INSTANCE);
 
 //   const [signature, _] = base58.deserialize(tx.signature);
