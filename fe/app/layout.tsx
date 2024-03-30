47d8f75a389a998c853d4fe65dfc93ff6ac55409
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-30 16:56:26 +0800

    Update layout

--- fe/app/layout.tsx
+++ fe/app/layout.tsx
@@ -8,7 +8,7 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { SOLANA } from "@/config";
-import Link from "next/link";
+import { AppBar, Sidenav } from "@/components";
 require("@solana/wallet-adapter-react-ui/styles.css");
 const inter = Inter({ subsets: ["latin"] });
 
@@ -31,28 +31,11 @@ export default function RootLayout({
             <WalletProvider wallets={[]} autoConnect>
               <WalletModalProvider>
                 <div className="flex">
-                  <div className="flex flex-col p-2 gap-2 min-h-screen bg-slate-900 w-[10vw]">
-                    <Link href={"/connect-wallet"}>Connect Wallet</Link>
-                    <Link href={"/umi/connect-wallet"}>UMI Connect Wallet</Link>
-                    <Link href={"/umi/create-mint"}>UMI Create Mint</Link>
-                    <Link href={"/umi/create-fungible-token"}>
-                      UMI Create Fungible Token
-                    </Link>
-                    <Link href={"/umi/upload-irys"}>UMI Upload Irys</Link>
-                    <Link href={"/umi/upload-json-irys"}>
-                      UMI Upload Json Irys
-                    </Link>
-                    <Link href={"/umi/create-nft"}>UMI Create NFT</Link>
-                    <Link href={"/umi/create-candy-machine"}>
-                      UMI Create Candy Machine
-                    </Link>
-                    <Link href={"/umi/create-merkle-tree"}>
-                      UMI Create Merkle Tree
-                    </Link>{" "}
-                    <Link href={"/umi/create-cnft"}>UMI cNFT</Link>
+                  <Sidenav />
+                  <div className="flex flex-col">
+                    <AppBar />
+                    <div className="min-h-[90vh] w-[90vw]">{children}</div>
                   </div>
-
-                  <div className="min-h-screen w-[90vw]">{children}</div>
                 </div>
               </WalletModalProvider>
             </WalletProvider>
