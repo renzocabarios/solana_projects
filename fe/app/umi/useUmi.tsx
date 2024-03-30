eb522d1cbd05670010510643b2892ee18c26ae77
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-30 17:37:29 +0800

    Update useUmi

--- fe/app/umi/useUmi.tsx
+++ fe/app/umi/useUmi.tsx
@@ -1,7 +1,10 @@
 import { SOLANA } from "@/config";
 import { mplBubblegum } from "@metaplex-foundation/mpl-bubblegum";
+import { mplCandyMachine } from "@metaplex-foundation/mpl-candy-machine";
+import { mplToolbox } from "@metaplex-foundation/mpl-toolbox";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 import { useWallet } from "@solana/wallet-adapter-react";
 
 function useUmi() {
@@ -9,6 +12,9 @@ function useUmi() {
 
   const umi = createUmi(SOLANA.rpc)
     .use(walletAdapterIdentity(wallet))
+    .use(irysUploader())
+    .use(mplToolbox())
+    .use(mplCandyMachine())
     .use(mplBubblegum());
 
   return umi;
