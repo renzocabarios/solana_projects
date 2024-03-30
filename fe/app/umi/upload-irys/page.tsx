3a248f5f724146d89b04e3ca29725f1f0a9e8c83
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-03-30 17:38:28 +0800

    Update UMIUploadIrys

--- fe/app/umi/upload-irys/page.tsx
+++ fe/app/umi/upload-irys/page.tsx
@@ -2,17 +2,13 @@
 
 import React from "react";
 import useUmi from "../useUmi";
-import { walletAdapterIdentity } from "@metaplex-foundation/umi-signer-wallet-adapters";
 import { useWallet } from "@solana/wallet-adapter-react";
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 import { createGenericFileFromBrowserFile } from "@metaplex-foundation/umi";
 
 function UMIUploadIrys() {
-  const wallet = useWallet();
   const umi = useUmi();
 
   const onUpload = async (e: any) => {
-    umi.use(walletAdapterIdentity(wallet)).use(irysUploader());
     const file = await createGenericFileFromBrowserFile(e.target.files[0]);
     const amount = await umi.uploader.getUploadPrice([file]);
     alert(Number(amount.basisPoints) / 10 ** amount.decimals);
