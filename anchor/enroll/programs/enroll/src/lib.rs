4b63f1302c50d91aff37579e03a60db7b8782888
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-04-01 14:24:38 +0800

    Update enroll

--- anchor/enroll/programs/enroll/src/lib.rs
+++ anchor/enroll/programs/enroll/src/lib.rs
@@ -25,17 +25,18 @@ pub struct Initialize<'info> {
         seeds = [b"enroll"], 
         bump,                
         payer = payer,
-        // 24 is the max space for type String
-        // TODO: change this to 4 + <string>.length in bytes for string feilds
-        space = 8 + 24 + 24 + 1
+        space = 8 + ExampleAccount::INIT_SPACE
     )]
     pub enrollment: Account<'info, Enrollment>,
     pub system_program: Program<'info, System>,
 }
 
 #[account]
+#[derive(InitSpace)]
 pub struct Enrollment {
+    #[max_len(50)]
     pub discord: String, // 4 + length of string in bytes
+    #[max_len(50)]
     pub github: String, // 4 + length of string in bytes
     pub bump: u8,   // 1 byte
 }
