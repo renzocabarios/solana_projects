6ccfef3eea1184e82274fb5594225f418d80f192
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-04-01 09:27:32 +0800

    Add irysUploader in umi

--- js_scripts/umi/config/index.ts
+++ js_scripts/umi/config/index.ts
@@ -4,11 +4,14 @@ import { keypairIdentity } from "@metaplex-foundation/umi";
 import { base58 } from "@metaplex-foundation/umi/serializers";
 import { mplToolbox } from "@metaplex-foundation/mpl-toolbox";
 import { mplBubblegum } from "@metaplex-foundation/mpl-bubblegum";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 
-const UMI_INSTANCE = createUmi(RPC_URL).use(mplToolbox()).use(mplBubblegum());
+const UMI_INSTANCE = createUmi(RPC_URL)
+  .use(mplToolbox())
+  .use(mplBubblegum())
+  .use(irysUploader());
 const decoded = base58.serialize(PAYER_PRIVATE_KEY);
 const keypair = UMI_INSTANCE.eddsa.createKeypairFromSecretKey(decoded);
-
 UMI_INSTANCE.use(keypairIdentity(keypair));
 
 export { UMI_INSTANCE };
