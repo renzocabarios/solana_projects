e20296b6ff20ad3e9b9df2c6c9cd6b46b764a543
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-04-30 15:23:48 +0800

    Update create-fungible-token

--- fe/app/umi/create-fungible-token/page.tsx
+++ fe/app/umi/create-fungible-token/page.tsx
@@ -1,58 +1,11 @@
-"use client";
-
-import React, { useState } from "react";
-import useUmi from "../useUmi";
-import { percentAmount, generateSigner } from "@metaplex-foundation/umi";
-import { base58 } from "@metaplex-foundation/umi/serializers";
-import { createFungible } from "@metaplex-foundation/mpl-token-metadata";
+import React from "react";
+import CreateFTForm from "./(components)/create-ft-form";
 
 function UMICreateFungibleToken() {
-  const [form, setform] = useState({ name: "", sellerFeeBasisPoints: 0 });
-  const umi = useUmi();
-
-  const onCreateMint = async () => {
-    const mint = generateSigner(umi);
-
-    const tx = await createFungible(umi, {
-      mint,
-      name: form.name,
-      // TODO: Create URI upload for this
-      uri: "https://example.com/my-fungible.json",
-      sellerFeeBasisPoints: percentAmount(form.sellerFeeBasisPoints),
-    }).sendAndConfirm(umi);
-
-    const [signature, _] = base58.deserialize(tx.signature);
-    alert(`Created Token ${mint.publicKey.toString()} `);
-    alert(`TX HASH ${signature}`);
-  };
-
-  const onHandleChange = (e: any) => {
-    const { name, value } = e.target;
-    setform((state) => ({ ...state, [name]: value }));
-  };
-
   return (
-    <div>
-      <div className="flex flex-col">
-        <p>Name</p>
-        <input
-          name="name"
-          type="text"
-          onChange={onHandleChange}
-          className="text-black"
-        />
-      </div>
-
-      <div className="flex flex-col">
-        <p>Seller Fee Basis Points</p>
-        <input
-          name="sellerFeeBasisPoints"
-          type="number"
-          onChange={onHandleChange}
-          className="text-black"
-        />
-      </div>
-      <button onClick={onCreateMint}>Create Mint</button>
+    <div className="flex flex-col gap-5">
+      <p className="text-xl font-bold">Create Fungible Token</p>
+      <CreateFTForm />
     </div>
   );
 }
