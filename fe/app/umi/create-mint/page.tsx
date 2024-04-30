4f4f2196acb411a93e9ca83165b8a63e2efd8ab4
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-04-30 15:08:33 +0800

    Update CreateMint

--- fe/app/umi/create-mint/page.tsx
+++ fe/app/umi/create-mint/page.tsx
@@ -1,45 +1,11 @@
-"use client";
-
-import React, { useState } from "react";
-import useUmi from "../useUmi";
-import { generateSigner } from "@metaplex-foundation/umi";
-import { base58 } from "@metaplex-foundation/umi/serializers";
-import { createMint } from "@metaplex-foundation/mpl-toolbox";
+import React from "react";
+import CreateMintForm from "./(components)/create-mint-form";
 
 function UMICreateMint() {
-  const [decimals, setdecimals] = useState(0);
-  const umi = useUmi();
-
-  const onCreateMint = async () => {
-    const mint = generateSigner(umi);
-    const tx = await createMint(umi, {
-      mint,
-      decimals,
-      mintAuthority: umi.identity.publicKey,
-      freezeAuthority: umi.identity.publicKey,
-    }).sendAndConfirm(umi);
-
-    const [signature, _] = base58.deserialize(tx.signature);
-
-    alert(
-      `Created Token ${mint.publicKey.toString()} with ${decimals} decimals`
-    );
-
-    alert(`TX HASH ${signature}`);
-  };
-
-  const onHandleChange = (e: any) => {
-    setdecimals(e.target.value);
-  };
-
   return (
-    <div>
-      <div className="flex flex-col">
-        <p>Decimals</p>
-        <input type="number" onChange={onHandleChange} className="text-black" />
-      </div>
-      <button onClick={onCreateMint}>Create Mint</button>
-    </div>
+    <>
+      <CreateMintForm />
+    </>
   );
 }
 
