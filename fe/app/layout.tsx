571e290e00819566cc537b5c4f99b31d89fc5e56
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-04-30 15:08:03 +0800

    Update RootLayout

--- fe/app/layout.tsx
+++ fe/app/layout.tsx
@@ -1,38 +1,10 @@
-"use client";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import { AppBar } from "@/components";
-import { Sidenav } from "@/components/sidenav";
-import Providers from "@/components/providers";
-import { Inter as FontSans } from "next/font/google";
-import { cn } from "@/lib/utils";
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+import Root from "./(components)/root";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        <Providers>
-          <div className="flex">
-            <Sidenav />
-            <div className="flex flex-col">
-              <AppBar />
-              <div className="min-h-[90vh] w-[90vw] p-5">{children}</div>
-            </div>
-          </div>
-        </Providers>
-      </body>
-    </html>
-  );
+  return <Root>{children}</Root>;
 }
