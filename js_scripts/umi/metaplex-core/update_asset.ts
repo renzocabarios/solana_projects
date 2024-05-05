37e2a4dc0a70ec449a3d7570244cf87c24ccfd40
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-05-05 19:35:02 +0800

    Add update_asset

---
+++ js_scripts/umi/metaplex-core/update_asset.ts
@@ -0,0 +1,34 @@
+import { UMI_INSTANCE } from "../config";
+import { generateSigner } from "@metaplex-foundation/umi";
+import { createV1, updateV1 } from "@metaplex-foundation/mpl-core";
+import { base58 } from "@metaplex-foundation/umi/serializers";
+import { fetchAssetV1 } from "@metaplex-foundation/mpl-core";
+const main = async () => {
+  const asset = generateSigner(UMI_INSTANCE);
+  const result = await createV1(UMI_INSTANCE, {
+    asset: asset,
+    name: "My Nft",
+    uri: "https://example.com/my-nft",
+  }).sendAndConfirm(UMI_INSTANCE, { confirm: { commitment: "finalized" } });
+
+  const [signature, _] = base58.deserialize(result.signature);
+
+  console.log(`SIGNATURE: ${signature}`);
+
+  const fetched = await fetchAssetV1(UMI_INSTANCE, asset.publicKey);
+  console.log(fetched);
+
+  const tx1 = await updateV1(UMI_INSTANCE, {
+    asset: asset.publicKey,
+    newName: "New Nft Name",
+    newUri: "https://example.com/new-uri",
+  }).sendAndConfirm(UMI_INSTANCE, { confirm: { commitment: "finalized" } });
+
+  const tx1Hash = base58.deserialize(tx1.signature);
+  console.log(`SIGNATURE: ${tx1Hash[0]}`);
+
+  const updated = await fetchAssetV1(UMI_INSTANCE, asset.publicKey);
+  console.log(updated);
+};
+
+main();
