3a2b140f069c3d35c3d917f410d377fdfa589f18
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-05-05 20:05:04 +0800

    Add candy-machine-core

---
+++ js_scripts/umi/candy-machine-core/create_candy_machine.ts
@@ -0,0 +1,46 @@
+import { UMI_INSTANCE } from "../config";
+import { generateSigner, publicKey, some } from "@metaplex-foundation/umi";
+import { base58 } from "@metaplex-foundation/umi/serializers";
+import {
+  Key,
+  createCollectionV1,
+  createV1,
+  getAssetV1GpaBuilder,
+  updateAuthority,
+} from "@metaplex-foundation/mpl-core";
+import { create } from "@metaplex-foundation/mpl-core-candy-machine";
+
+const candyMachine = generateSigner(UMI_INSTANCE);
+const main = async () => {
+  const asset = generateSigner(UMI_INSTANCE);
+  const collection = generateSigner(UMI_INSTANCE);
+  const tx1 = await createCollectionV1(UMI_INSTANCE, {
+    collection: collection,
+    name: "My NFT",
+    uri: "https://example.com/my-nft.json",
+  }).sendAndConfirm(UMI_INSTANCE, { confirm: { commitment: "finalized" } });
+
+  const tx2 = await (
+    await create(UMI_INSTANCE, {
+      candyMachine,
+      collection: collection.publicKey,
+      collectionUpdateAuthority: UMI_INSTANCE.identity,
+      itemsAvailable: 5000,
+      configLineSettings: some({
+        prefixName: "Example Asset #",
+        nameLength: 15,
+        prefixUri: "https://example.com/metadata/",
+        uriLength: 29,
+        isSequential: false,
+      }),
+    })
+  ).sendAndConfirm(UMI_INSTANCE, { confirm: { commitment: "finalized" } });
+
+  const tx1Hash = base58.deserialize(tx1.signature);
+  console.log(tx1Hash[0]);
+
+  const tx2Hash = base58.deserialize(tx2.signature);
+  console.log(tx2Hash[0]);
+};
+
+main();
