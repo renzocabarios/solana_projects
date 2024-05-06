3d2209ad39e73ac90f961d1119262c33816cf4b5
renzocabarios <renzo.cabarios@gmail.com>
2024-05-06 15:48:32 +0800

    Update lib

--- anchor/enroll/programs/enroll/src/lib.rs
+++ anchor/enroll/programs/enroll/src/lib.rs
@@ -1,42 +1,19 @@
 use anchor_lang::prelude::*;
 
+pub mod states;
+use crate::instructions::*;
+pub mod instructions;
 declare_id!("FDDbT6pAmifx3GffUhq67FZBEZp1ggYmhdiWwAoJRY5T");
 
 #[program]
 pub mod enroll {
     use super::*;
 
-   pub fn initialize(ctx: Context<Initialize>, discord: String, github: String) -> Result<()> {
-        let enrollment = &mut ctx.accounts.enrollment;
-        enrollment.discord = discord;
-        enrollment.github = github;
-        Ok(())
+    pub fn initialize(
+        ctx: Context<InitializeEnrollment>,
+        discord: String,
+        github: String,
+    ) -> Result<()> {
+        initialize_enrollment(ctx, discord, github)
     }
-}
-
-#[derive(Accounts)]
-#[instruction(content: String)]
-pub struct Initialize<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-
-    #[account(
-        init,
-        seeds = [b"enroll"], 
-        bump,                
-        payer = payer,
-        space = 8 + ExampleAccount::INIT_SPACE
-    )]
-    pub enrollment: Account<'info, Enrollment>,
-    pub system_program: Program<'info, System>,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Enrollment {
-    #[max_len(50)]
-    pub discord: String, // 4 + length of string in bytes
-    #[max_len(50)]
-    pub github: String, // 4 + length of string in bytes
-    pub bump: u8,   // 1 byte
 }
