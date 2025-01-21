4bf3ca604ae5c3cab75eee141beaefee71894747
renzocabarios <renzo.cabarios@gmail.com>
2025-01-21 10:42:00 +0800

    Update tests

--- solana_developer_bootcamp/lending_program/tests/main.test.ts
+++ solana_developer_bootcamp/lending_program/tests/main.test.ts
@@ -1,46 +1,32 @@
 import { BN } from "@coral-xyz/anchor";
 import { Keypair } from "@solana/web3.js";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
-import { createMint, getTreasuryAccountPda, mintTo } from "./utils";
+import {
+  createATA,
+  createMint,
+  getAssociatedTokenAccountPda,
+  getBankAccountPda,
+  getTreasuryAccountPda,
+  getUserAccountPda,
+  mintTo,
+} from "./utils";
 import { startEnv } from "./setups";
 
-describe("My test", () => {
+describe("lending program", () => {
   test("Test init user", async () => {
-    const { user, client } = await startEnv();
-
-    const usdc = Keypair.generate();
-
-    await createMint(
-      client,
-      user.account,
-      user.account.publicKey,
-      null,
-      6,
-      usdc
-    );
-
+    const { user, usdc } = await startEnv();
     await user.program.methods.initUser(usdc.publicKey).rpc();
   });
 
   test("Test init bank", async () => {
-    const { user, client } = await startEnv();
-
-    const usdc = Keypair.generate();
-
-    await createMint(
-      client,
-      user.account,
-      user.account.publicKey,
-      null,
-      6,
-      usdc
-    );
+    const { admin, client, usdc } = await startEnv();
 
     const treasuryAccountPDA = getTreasuryAccountPda(
       usdc.publicKey,
-      user.program.programId
+      admin.program.programId
     );
-    await user.program.methods
+
+    await admin.program.methods
       .initBank(new BN(1), new BN(1))
       .accounts({
         mint: usdc.publicKey,
@@ -48,14 +34,89 @@ describe("My test", () => {
       })
       .rpc();
 
-    const amount = 10_000 * 10 ** 9;
-    const mintTx = await mintTo(
+    const amount = 10_000 * 10 ** 6;
+    await mintTo(
       client,
-      user.account,
+      admin.account,
       usdc.publicKey,
       treasuryAccountPDA,
-      user.account.publicKey,
+      admin.account.publicKey,
       amount
     );
+  });
+
+  test("Deposit", async () => {
+    const { user, client, usdc, admin } = await startEnv();
+    await user.program.methods.initUser(usdc.publicKey).rpc();
+
+    const treasuryAccountPDA = getTreasuryAccountPda(
+      usdc.publicKey,
+      admin.program.programId
+    );
+    const bankAccountPDA = getBankAccountPda(
+      usdc.publicKey,
+      admin.program.programId
+    );
+
+    const userAccountPDA = getUserAccountPda(
+      user.account.publicKey,
+      admin.program.programId
+    );
+
+    await admin.program.methods
+      .initBank(new BN(1), new BN(1))
+      .accounts({
+        mint: usdc.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .rpc();
+
+    const amount = 10_000 * 10 ** 6;
+    const mintTx = await mintTo(
+      client,
+      admin.account,
+      usdc.publicKey,
+      treasuryAccountPDA,
+      admin.account.publicKey,
+      amount
+    );
+
+    const userAssociatedTokenAccountPDA = getAssociatedTokenAccountPda(
+      user.account.publicKey,
+      usdc.publicKey
+    );
+
+    await createATA(
+      client,
+      user.account,
+      userAssociatedTokenAccountPDA,
+      user.account.publicKey,
+      usdc.publicKey,
+      user.account
+    );
+
+    await mintTo(
+      client,
+      admin.account,
+      usdc.publicKey,
+      userAssociatedTokenAccountPDA,
+      admin.account.publicKey,
+      amount
+    );
+
+    await user.program.methods
+      .deposit(new BN(1 * 10 ** 6))
+      .accountsPartial({
+        mint: usdc.publicKey,
+        bank: bankAccountPDA,
+        bankTokenAccount: treasuryAccountPDA,
+        userAccount: userAccountPDA,
+        userTokenAccount: userAssociatedTokenAccountPDA,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .rpc();
+
+    console.log(await user.program.account.bank.fetch(bankAccountPDA));
+    console.log(await user.program.account.user.fetch(userAccountPDA));
   });
 });
